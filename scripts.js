a84d958e133ba08910cae85ddc920c92979f87d7
Judson777 <judsonjuvieira@gmail.com>
2025-03-18 12:16:24 -0300

    Primeiro commit - starbucks

---
+++ scripts.js
@@ -0,0 +1,11 @@
+let circulo = document.querySelector(".circulo")
+let img = document.querySelector(".img-copo")
+
+
+function trocarACor(cor){
+    circulo.style.background = cor
+}
+
+function trocarImagem(endereco){
+    img.src = endereco
+}
